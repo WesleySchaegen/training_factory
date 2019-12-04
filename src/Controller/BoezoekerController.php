cacb460dc49e7e1f7af816bc1e9f48fa52328c13
Wesley <302054575@student.rocmondriaan.nl>
2019-12-04 14:35:46 +0100

    all visitor links added

--- src/Controller/BoezoekerController.php
+++ src/Controller/BoezoekerController.php
@@ -11,11 +11,39 @@ use Symfony\Component\Routing\Annotation\Route;
 class BoezoekerController extends AbstractController
 {
     /**
-     * @Route("/", name="app_homepage")
+     * @Route("/", name="app_bezoek_homepage")
      */
 
     public function homepage()
     {
-        return $this->render('screen/algemeen.html.twig');
+        return $this->render('screen/bezoeker/home.html.twig');
+    }
+    /**
+     * @Route("/bezoeker/aanbod", name="app_bezoek_aanbod")
+     */
+    public function aanbod()
+    {
+        return $this->render('screen/bezoeker/trainingbod.html.twig');
+    }
+    /**
+     * @Route("/bezoeker/registreren", name="app_bezoek_lidword")
+     */
+    public function lidword()
+    {
+        return $this->render('screen/bezoeker/registreren.html.twig');
+    }
+    /**
+     * @Route("/bezoeker/gedragsregels", name="app_bezoek_gregels")
+     */
+    public function gregels()
+    {
+        return $this->render('screen/bezoeker/gedragregels.html.twig');
+    }
+    /**
+     * @Route("/bezoeker/lokatie&contact", name="app_bezoek_locatie")
+     */
+    public function locatie()
+    {
+        return $this->render('screen/bezoeker/lokatiecontact.html.twig');
     }
 }
