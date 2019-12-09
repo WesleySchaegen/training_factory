ced4c733d4f890a6a8fb510d6ff8e5a483959835
Wesley <302054575@student.rocmondriaan.nl>
2019-12-09 14:29:57 +0100

    endday

--- src/Controller/BoezoekerController.php
+++ src/Controller/BoezoekerController.php
@@ -4,12 +4,15 @@
 namespace App\Controller;
 
 
+use App\Form\RegisFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BoezoekerController extends AbstractController
 {
+
     /**
      * @Route("/", name="app_bezoek_homepage")
      */
