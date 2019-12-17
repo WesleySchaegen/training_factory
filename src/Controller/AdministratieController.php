76771a2771e0272d1af1ba948b6ff9666b47beb0
Wesley <302054575@student.rocmondriaan.nl>
2019-12-17 12:57:52 +0100

    further have I come

--- src/Controller/AdministratieController.php
+++ src/Controller/AdministratieController.php
@@ -15,6 +15,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdministratieController extends AbstractController
 {
     /**
+     * @Route("/admin/home", name="app_admin_homepage")
+     */
+
+    public function adhomepage()
+    {
+        return $this->render('screen/admin/home.html.twig');
+    }
+
+    /**
+     * @Route("/admin/instructeurs", name="app_admin_instructeurs")
+     */
+
+    public function adinstructeurs()
+    {
+        return $this->render('screen/admin/instructeurs.html.twig');
+    }
+
+    /**
+ * @Route("/admin/leden", name="app_admin_leden")
+ */
+
+    public function adleden()
+    {
+        return $this->render('screen/admin/leden.html.twig');
+    }
+
+    /**
+     * @Route("/admin/training", name="app_admin_training")
+     */
+
+    public function adtraining()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $trainingen = $em->getRepository(Training::class)->findAll();
+        return $this->render('screen/admin/trainingpage.html.twig',["trainingen"=>$trainingen]);
+    }
+
+    /**
      * @Route("/admin/trainingen", name="app_admin_trainingen")
      */
     public function newTraining(Request $request, EntityManagerInterface $em)
@@ -30,6 +68,6 @@ class AdministratieController extends AbstractController
         }
 
         return $this->render('screen/admin/trainingen.html.twig',
-            ['regisForm' => $form->createView()]);
+            ['trainForm' => $form->createView()]);
     }
 }
